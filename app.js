774e6d3683c69f2066178c7c0c5621ca1af6a193
Keyan <keyantabor98@gmail.com>
2022-09-21 22:24:36 -0400

    add javascript to website

---
+++ app.js
@@ -0,0 +1,17 @@
+let answer=("Kirby")
+
+let userName= prompt("What is your name?")
+document.write("Welcome " + userName + "!")
+
+let theirAnswerGoesHere = prompt("What was the best game of 2022")
+
+    if(theirAnswerGoesHere== "Kirby")
+        alert("Correct!")
+    else 
+        alert("Wrong")
+
+function changeBackgroundWhite(){
+    document.body.style.background = "red";
+    document.body.style.text ="white"
+    }
+
