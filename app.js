a7c502a2025aaa34a9a14cbd32892b293f3076f5
Keyan <keyantabor98@gmail.com>
2022-09-22 19:09:13 -0400

    sanity check because VSC keeps crashing

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-let answer=("Kirby")
-
-let userName= prompt("What is your name?")
-document.write("Welcome " + userName + "!")
-
-let theirAnswerGoesHere = prompt("What was the best game of 2022")
-
-    if(theirAnswerGoesHere== "Kirby")
-        alert("Correct!")
-    else 
-        alert("Wrong")
+function guessColor(){
+  let usersGuess = prompt('what is my fav color?');
+  
+  while (usersGuess !== 'yellow'){
+    usersGuess = prompt('Wrong! Guess again');
+  }
+}
+guessColor();
 
 function myFunction() {
    var element = document.body;
